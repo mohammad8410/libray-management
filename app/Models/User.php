522ca8815bfe13b7db9fef8fa712fe795861b5c3
mrt <m8mrt38@gmail.com>
2023-05-25 15:14:22 +0330

    User and UserScore Relations changed to one to many

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -52,9 +51,9 @@ class User extends Authenticatable
         return $this->belongsToMany(Book::class);
     }
 
-    public function score(): HasOne
+    public function score(): HasMany
     {
-        return $this->hasOne(UserScore::class);
+        return $this->hasMany(UserScore::class);
     }
 
 }
