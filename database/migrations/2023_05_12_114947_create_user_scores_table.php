b3331145f0d3d5ff16be0fec34740e0d589e24cf
mrt <m8mrt38@gmail.com>
2023-05-25 15:15:03 +0330

    description column added to user score table

--- database/migrations/2023_05_12_114947_create_user_scores_table.php
+++ database/migrations/2023_05_12_114947_create_user_scores_table.php
@@ -7,10 +7,12 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('user_scores', function (Blueprint $table) {
+        Schema::create('user_scores', function (Blueprint $table)
+        {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->integer('score');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
